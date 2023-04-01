23521334bc060c1b3b42d1bfaafbad738efa0f8b
TheLokin <diego.ramil.lopez@gmail.com>
2023-04-01 21:34:47 +0200

    Add marker type model

--- app/src/main/java/com/temtem/interactive/map/temzone/markers/Marker.kt
+++
@@ -1,8 +0,0 @@
-package com.temtem.interactive.map.temzone.markers
-
-import android.annotation.SuppressLint
-import android.content.Context
-import androidx.appcompat.widget.AppCompatImageView
-
-@SuppressLint("ViewConstructor")
-class Marker(context: Context, val x: Double, val y: Double) : AppCompatImageView(context)
---
+++ app/src/main/java/com/temtem/interactive/map/temzone/model/MarkerType.kt
@@ -0,0 +1,6 @@
+package com.temtem.interactive.map.temzone.model
+
+enum class MarkerType {
+    SPAWN,
+    SAIPARK
+}
