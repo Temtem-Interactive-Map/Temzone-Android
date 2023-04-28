af1fb1f0d73e4ab5e3455ce605d2e68f31d16ae0
TheLokin <diego.ramil.lopez@gmail.com>
2023-04-28 20:45:53 +0200

    Fix system ui visibility

--- app/src/main/java/com/temtem/interactive/map/temzone/ui/MainActivity.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/ui/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : AppCompatActivity() {
 
         window.apply {
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             statusBarColor = Color.TRANSPARENT
         }
 
