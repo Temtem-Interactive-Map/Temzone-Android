0a232688118b16ecf05051096d12132e43c1924a
TheLokin <diego.ramil.lopez@gmail.com>
2023-05-18 12:59:01 +0200

    Add settings fragment

---
+++ app/src/main/java/com/temtem/interactive/map/temzone/ui/settings/SettingsFragment.kt
@@ -0,0 +1,47 @@
+package com.temtem.interactive.map.temzone.ui.settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialSharedAxis
+import com.temtem.interactive.map.temzone.R
+import com.temtem.interactive.map.temzone.databinding.SettingsFragmentBinding
+import com.temtem.interactive.map.temzone.utils.bindings.viewBindings
+
+class SettingsFragment : Fragment(R.layout.settings_fragment) {
+
+    private val viewBinding: SettingsFragmentBinding by viewBindings()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowInsetsControllerCompat(
+            requireActivity().window,
+            requireActivity().window.decorView
+        ).apply {
+            isAppearanceLightStatusBars = true
+        }
+
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewBinding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        WindowInsetsControllerCompat(
+            requireActivity().window,
+            requireActivity().window.decorView
+        ).apply {
+            isAppearanceLightStatusBars = true
+        }
+    }
+}
