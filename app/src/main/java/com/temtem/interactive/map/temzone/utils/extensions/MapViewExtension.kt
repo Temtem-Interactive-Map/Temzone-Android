1527fe5ee1b17fd8d4161d20ce8e1a90df11a1eb
TheLokin <diego.ramil.lopez@gmail.com>
2023-05-19 03:22:11 +0200

    Update map view extension

--- app/src/main/java/com/temtem/interactive/map/temzone/utils/extensions/MapViewExtension.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/utils/extensions/MapViewExtension.kt
@@ -10,11 +10,19 @@ class MarkerView(
     context: Context, val id: String, val x: Double, val y: Double
 ) : AppCompatImageView(context)
 
+/**
+ * Scrolls the MapView so it's centered on the x and y coordinates.
+ * Takes an additional [destinationScale] parameter.
+ *
+ * The scroll position and scale are animated if [shouldAnimate] is set to `true`.
+ *
+ * @param x The x coordinate in the MapView's coordinate system.
+ * @param y The y coordinate in the MapView's coordinate system.
+ * @param destinationScale The scale of the MapView when centered on the marker
+ * @param shouldAnimate True if the movement should use a transition effect.
+ */
 fun MapView.moveToPosition(
-    x: Double,
-    y: Double,
-    destinationScale: Float,
-    shouldAnimate: Boolean
+    x: Double, y: Double, destinationScale: Float, shouldAnimate: Boolean
 ) {
     val scaledX = (x * destinationScale).toInt()
     val scaledY = (y * destinationScale).toInt()
