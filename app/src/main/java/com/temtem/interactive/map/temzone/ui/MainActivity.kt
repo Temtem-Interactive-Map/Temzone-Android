0b0a91b5c1de4ce7ba06c28d7c8391fabae0b16f
TheLokin <diego.ramil.lopez@gmail.com>
2023-05-26 13:03:37 +0200

    Add splash screen

--- app/src/main/java/com/temtem/interactive/map/temzone/ui/MainActivity.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/ui/MainActivity.kt
@@ -2,12 +2,15 @@ package com.temtem.interactive.map.temzone.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.temtem.interactive.map.temzone.R
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
