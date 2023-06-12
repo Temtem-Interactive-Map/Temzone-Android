a2e6c5b564dd94abdf40dc67bcfe48f184f77448
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-12 04:18:30 +0200

    Update extensions

--- app/src/main/java/com/temtem/interactive/map/temzone/core/extension/ContextExtension.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/core/extension/ContextExtension.kt
@@ -1,6 +1,11 @@
 package com.temtem.interactive.map.temzone.core.extension
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.datastore: DataStore<Preferences> by preferencesDataStore("settings")
 
 fun Context.dpToPx(dp: Int): Int {
     return (dp * resources.displayMetrics.density).toInt()
--- app/src/main/java/com/temtem/interactive/map/temzone/core/extension/FragmentExtension.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/core/extension/FragmentExtension.kt
@@ -1,7 +1,12 @@
 package com.temtem.interactive.map.temzone.core.extension
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 fun Fragment.closeKeyboard() {
@@ -9,4 +14,19 @@ fun Fragment.closeKeyboard() {
         requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
     inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+}
+
+fun Fragment.hasNotificationPermission(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.POST_NOTIFICATIONS,
+        ) == PackageManager.PERMISSION_GRANTED
+    } else true
+}
+
+fun Fragment.requestNotificationPermission(requestPermissionLauncher: ActivityResultLauncher<String>) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && !hasNotificationPermission()) {
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
 }
