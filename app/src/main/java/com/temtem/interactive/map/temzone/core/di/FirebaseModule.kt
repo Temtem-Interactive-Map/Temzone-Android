b000c7486301d054e00c749c317b6fe7f0c8607f
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-12 04:18:12 +0200

    Update firebase module

--- app/src/main/java/com/temtem/interactive/map/temzone/core/di/FirebaseModule.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/core/di/FirebaseModule.kt
@@ -6,6 +6,8 @@ import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,5 +28,11 @@ object FirebaseModule {
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth {
         return Firebase.auth
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseMessaging(): FirebaseMessaging {
+        return Firebase.messaging
     }
 }
