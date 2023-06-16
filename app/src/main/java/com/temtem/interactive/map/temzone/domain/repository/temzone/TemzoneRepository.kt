f5ac7ed1bb00cbf3245dafc5c04389f7f4d182c8
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-17 00:55:32 +0200

    Update temzone repository

--- app/src/main/java/com/temtem/interactive/map/temzone/domain/repository/temzone/TemzoneRepository.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/domain/repository/temzone/TemzoneRepository.kt
@@ -1,14 +1,15 @@
 package com.temtem.interactive.map.temzone.domain.repository.temzone
 
-import com.temtem.interactive.map.temzone.domain.repository.temzone.model.Page
+import androidx.paging.PagingData
 import com.temtem.interactive.map.temzone.domain.repository.temzone.model.marker.Marker
 import com.temtem.interactive.map.temzone.domain.repository.temzone.model.marker.saipark.MarkerSaipark
 import com.temtem.interactive.map.temzone.domain.repository.temzone.model.marker.spawn.MarkerSpawn
+import kotlinx.coroutines.flow.Flow
 
 interface TemzoneRepository {
     suspend fun getMarkers(): List<Marker>
     suspend fun getMarkerSpawn(id: String): MarkerSpawn
     suspend fun setTemtemObtained(id: Int)
     suspend fun getMarkerSaipark(id: String): MarkerSaipark
-    suspend fun searchMarkers(query: String, limit: Int, offset: Int): Page<Marker>
+    fun searchMarkers(query: String): Flow<PagingData<Marker>>
 }
