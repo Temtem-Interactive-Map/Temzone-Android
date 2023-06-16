7157f1244a537a79cd6fdf86ec272c6786a98093
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-17 00:56:00 +0200

    Add notification channel

--- app/src/main/java/com/temtem/interactive/map/temzone/ui/MainApplication.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/ui/MainApplication.kt
@@ -1,7 +1,28 @@
 package com.temtem.interactive.map.temzone.ui
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import com.temtem.interactive.map.temzone.R
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+
+    companion object {
+        const val CHANNEL_ID = "TEMZONE_CHANNEL"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            getString(R.string.app_name),
+            NotificationManager.IMPORTANCE_DEFAULT,
+        )
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+    }
+}
