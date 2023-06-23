339e0d205274aeb7200270a272c979a3e646e59b
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-23 20:29:40 +0200

    Update map view extension

--- app/src/main/java/com/temtem/interactive/map/temzone/core/extension/MapViewExtension.kt
+++ app/src/main/java/com/temtem/interactive/map/temzone/core/extension/MapViewExtension.kt
@@ -2,119 +2,39 @@ package com.temtem.interactive.map.temzone.core.extension
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import com.temtem.interactive.map.temzone.domain.repository.temzone.model.marker.Marker
-import kotlinx.coroutines.delay
 import ovh.plrapps.mapview.MapView
-import ovh.plrapps.mapview.api.addMarker
-import ovh.plrapps.mapview.api.getMarkerByTag
 
 @SuppressLint("ViewConstructor")
 class MarkerView(
     context: Context,
     val id: String,
+    val type: Marker.Type,
     val x: Double,
     val y: Double,
-) : AppCompatImageView(context)
+) : AppCompatImageView(context) {
 
-/**
- * Adds a marker to the MapView.
- *
- * @param marker The marker to add.
- */
-suspend fun MapView.addMarker(marker: Marker) {
-    val view = getMarkerByTag(marker.id) as MarkerView?
+    companion object {
+        fun fromMarker(marker: Marker, context: Context): MarkerView {
+            return MarkerView(
+                context,
+                marker.id,
+                marker.type,
+                marker.x.toDouble(),
+                marker.y.toDouble(),
+            ).apply {
+                // Set the marker's elevation to its hash code so
+                // the markers are drawn always in the same order
+                elevation = marker.id.hashCode().toFloat()
 
-    if (view == null) {
-        val markerView = MarkerView(
-            context,
-            marker.id,
-            marker.x.toDouble(),
-            marker.y.toDouble(),
-        ).apply {
-            // Set the marker's elevation to its hash code so the markers are drawn
-            // always
-            // in the same order
-            elevation = marker.id.hashCode().toFloat()
+                val drawable = marker.getDrawable(context).apply {
+                    alpha = if (marker.obtained) 153 else 255
+                }
 
-            // Set the marker's visibility
-            visibility = View.VISIBLE
-
-            // Set the marker's drawable
-            val drawable = marker.getDrawable(context).apply {
-                alpha = if (marker.obtained) 153 else 255
+                setImageDrawable(drawable)
             }
-
-            // Set the drawable to the marker view
-            setImageDrawable(drawable)
         }
-
-        // Add the marker to the map
-        addMarker(
-            markerView,
-            markerView.x,
-            markerView.y,
-            -0.5f,
-            -0.5f,
-            0f,
-            0f,
-            markerView.id,
-        )
-        delay(1)
-    } else {
-        view.visibility = View.VISIBLE
-
-        // If the marker already exists in the map, update its opacity
-        view.drawable.alpha = if (marker.obtained) 153 else 255
-    }
-}
-
-/**
- * Removes a marker from the MapView.
- *
- * @param marker The marker to remove.
- */
-suspend fun MapView.removeMarker(marker: Marker) {
-    val view = getMarkerByTag(marker.id) as MarkerView?
-
-    if (view == null) {
-        val markerView = MarkerView(
-            context,
-            marker.id,
-            marker.x.toDouble(),
-            marker.y.toDouble(),
-        ).apply {
-            // Set the marker's elevation to its hash code so the
-            // markers are drawn always in the same order
-            elevation = marker.id.hashCode().toFloat()
-
-            // Set the marker's visibility
-            visibility = View.INVISIBLE
-
-            // Set the marker's drawable
-            val drawable = marker.getDrawable(context).apply {
-                alpha = if (marker.obtained) 153 else 255
-            }
-
-            // Set the drawable to the marker view
-            setImageDrawable(drawable)
-        }
-
-        // Add the marker to the map
-        addMarker(
-            markerView,
-            markerView.x,
-            markerView.y,
-            -0.5f,
-            -0.5f,
-            0f,
-            0f,
-            markerView.id,
-        )
-        delay(1)
-    } else {
-        view.visibility = View.INVISIBLE
     }
 }
 
